40f2390f01a30735aeb9e1b39da0d56f765ad585
Alexander Urban <lexa@toxa.de>
2021-02-14 12:51:06 +0100

    Processor "TitleBasic" revised: Indexing in ElasticSearch during import

--- src/main/java/se/cygni/mss/tsv/jobs/TitleBasicBatchConfiguration.java
+++ src/main/java/se/cygni/mss/tsv/jobs/TitleBasicBatchConfiguration.java
@@ -1,11 +1,18 @@
 package se.cygni.mss.tsv.jobs;
 
+// import org.apache.http.util.EntityUtils;
+// import org.elasticsearch.client.Request;
+// import org.elasticsearch.client.Response;
+// import org.elasticsearch.client.RestClient;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.step.builder.SimpleStepBuilder;
+import org.springframework.batch.core.step.tasklet.TaskletStep;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.file.FlatFileItemReader;
@@ -32,7 +39,8 @@ public class TitleBasicBatchConfiguration {
     private StepBuilderFactory stepBuilderFactory;
     @Autowired
     private DataSource dataSource;
-
+    // @Autowired
+    // private RestClient elasticSearchClient;
 
     // tag::readerwriterprocessor[]
     @Bean
@@ -79,19 +87,19 @@ public class TitleBasicBatchConfiguration {
         JdbcBatchItemWriter<TitleBasic> writer = new JdbcBatchItemWriter<>();
         writer.setItemSqlParameterSourceProvider(
                 new BeanPropertyItemSqlParameterSourceProvider<>());
-        writer.setSql("DROP TABLE IF EXISTS title_basic;");
-        writer.setSql("CREATE TABLE title_basic  (\n" +
-                "    title_id BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY,\n" +
-                "    tconst VARCHAR(20),\n" +
-                "    title_type VARCHAR(20),\n" +
-                "    primary_title VARCHAR(255),\n" +
-                "    original_title VARCHAR(255),\n" +
-                "    is_adult VARCHAR(20),\n" +
-                "    start_year VARCHAR(20),\n" +
-                "    end_year VARCHAR(20),\n" +
-                "    runtime_minutes VARCHAR(20),\n" +
-                "    genres VARCHAR(255)\n" +
-                ")ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;");
+        // writer.setSql("DROP TABLE IF EXISTS title_basic;");
+        // writer.setSql("CREATE TABLE title_basic  (\n" +
+        //         "    title_id BIGINT AUTO_INCREMENT NOT NULL PRIMARY KEY,\n" +
+        //         "    tconst VARCHAR(20),\n" +
+        //         "    title_type VARCHAR(20),\n" +
+        //         "    primary_title VARCHAR(255),\n" +
+        //         "    original_title VARCHAR(255),\n" +
+        //         "    is_adult VARCHAR(20),\n" +
+        //         "    start_year VARCHAR(20),\n" +
+        //         "    end_year VARCHAR(20),\n" +
+        //         "    runtime_minutes VARCHAR(20),\n" +
+        //         "    genres VARCHAR(255)\n" +
+        //         ")ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;");
         writer.setSql("INSERT INTO title_basic (" +
                 "tconst, " +
                 "title_type, " +
@@ -113,6 +121,17 @@ public class TitleBasicBatchConfiguration {
                 ":runtimeMinutes, " +
                 ":genres)");
         writer.setDataSource(dataSource);
+
+        // try {
+        //   Request request = new Request("GET", "/");
+        //   request.addParameter("pretty", "true");
+        //   Response response = elasticSearchClient.performRequest(request);
+        //   String responseBody = EntityUtils.toString(response.getEntity());
+        //   System.out.println("Elastic Search root query:" + responseBody);
+        // } catch (Exception error) {
+        //   System.out.println("Elastic Search root query failed:" + error.getMessage());
+        // }
+      
         return writer;
     }
     // end::readerwriterprocessor[]
@@ -121,14 +140,28 @@ public class TitleBasicBatchConfiguration {
     // tag::jobstep[]
     @Bean
     public Job importTitleBasicJob(ImportTitleBasicsCompletionNotificationListener listener) {
-        return jobBuilderFactory.get("importTitleBasicJob").incrementer(new RunIdIncrementer())
-                .listener(listener).flow(buildFactoryForTitleBasics()).end().build();
+      JobBuilder jobBuilder = jobBuilderFactory
+        .get("importTitleBasicJob")
+        .incrementer(new RunIdIncrementer())
+        .listener(listener);
+      Job job = jobBuilder
+        .flow(buildFactoryForTitleBasics())
+        .end()
+        .build();
+      return job;
     }
 
     @Bean
     public Step buildFactoryForTitleBasics() {
-        return stepBuilderFactory.get("buildFactoryForTitleBasics").<TitleBasic, TitleBasic>chunk(10).reader(readerTitleBasic())
-                .processor(processorTitleBasic()).writer(writerTitleBasic()).build();
+      SimpleStepBuilder<TitleBasic, TitleBasic> simpleStepBuilder = stepBuilderFactory
+        .get("buildFactoryForTitleBasics")
+        .<TitleBasic, TitleBasic>chunk(10);
+      TaskletStep step = simpleStepBuilder
+        .reader(readerTitleBasic())
+        .processor(processorTitleBasic())
+        .writer(writerTitleBasic())
+        .build();
+      return step;
     }
     // end::jobstep[]
 }
--- src/main/java/se/cygni/mss/tsv/processor/TitleBasicItemProcessor.java
+++ src/main/java/se/cygni/mss/tsv/processor/TitleBasicItemProcessor.java
@@ -1,21 +1,53 @@
 package se.cygni.mss.tsv.processor;
 
 import lombok.extern.slf4j.Slf4j;
+
+import com.google.gson.Gson;
+
+import org.elasticsearch.client.Request;
+import org.elasticsearch.client.Response;
+import org.elasticsearch.client.ResponseListener;
+import org.elasticsearch.client.RestClient;
 import org.springframework.batch.item.ItemProcessor;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import se.cygni.mss.tsv.model.TitleBasic;
 
 @Slf4j
 public class TitleBasicItemProcessor implements ItemProcessor<TitleBasic, TitleBasic> {
 
-    @Override
-    public TitleBasic process(TitleBasic titleBasic) throws Exception {
-        final String tconst = titleBasic.getTconst();
-        final String primaryTitle = titleBasic.getPrimaryTitle();
-        final String originalTitle = titleBasic.getOriginalTitle();
-        final String genres = titleBasic.getGenres();
-        final TitleBasic transformedTitleBasic = new TitleBasic(tconst, primaryTitle, originalTitle, genres);
-        log.info("Converting (" + titleBasic + ") into (" + transformedTitleBasic + ")");
+  @Autowired
+  private RestClient elasticSearchClient;
 
-        return transformedTitleBasic;
-    }
+  @Autowired
+  private Gson gson;
+
+  @Override
+  public TitleBasic process(TitleBasic titleBasic) throws Exception {
+    Request request = new Request("POST", "/title-basic/_doc");
+    request.setJsonEntity(gson.toJson(titleBasic));
+
+    elasticSearchClient.performRequestAsync(request, new ResponseListener() {
+      @Override
+      public void onSuccess(Response response) {
+        log.info("*** Elastic *** index written: " + titleBasic.getPrimaryTitle());
+      }
+
+      @Override
+      public void onFailure(Exception error) {
+        log.info("*** Elastic *** ERROR writing index: " + error.getMessage());
+      }
+    });
+    
+    return titleBasic;
+
+    // final String tconst = titleBasic.getTconst();
+    // final String primaryTitle = titleBasic.getPrimaryTitle();
+    // final String originalTitle = titleBasic.getOriginalTitle();
+    // final String genres = titleBasic.getGenres();
+    // final TitleBasic transformedTitleBasic = new TitleBasic(tconst, primaryTitle, originalTitle, genres);
+    // log.info("Converting (" + titleBasic + ") into (" + transformedTitleBasic + ")");
+
+    // return transformedTitleBasic;
+  }
 }
