f61c601c7909b2249c505812c780bef3e4503098
Alexander Urban <lexa@toxa.de>
2021-02-14 12:50:33 +0100

    Startup revised: Showing ElasticSearch stats

--- src/main/java/se/cygni/mss/config/Config.java
+++ src/main/java/se/cygni/mss/config/Config.java
@@ -1,13 +1,20 @@
 package se.cygni.mss.config;
 
+import lombok.extern.slf4j.Slf4j;
+
 import com.google.gson.Gson;
 
 import org.apache.http.HttpHost;
+import org.apache.http.util.EntityUtils;
+import org.elasticsearch.client.Request;
+import org.elasticsearch.client.Response;
+import org.elasticsearch.client.ResponseListener;
 import org.elasticsearch.client.RestClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 public class Config {
 
@@ -20,14 +27,42 @@ public class Config {
   @Value("${elastic.protocol}")
   private String elasticProtocol;
 
+  private Gson gson;
+
+  public Config() {
+      gson = new Gson();
+  }
+
   @Bean(destroyMethod = "close")
   public RestClient getElasticSearchClient() {
     RestClient restClient = RestClient.builder(new HttpHost(elasticHostname, elasticPort, elasticProtocol)).build();
+
+    Request request = new Request("GET", "/_cat/indices");
+    request.addParameter("v", "true");
+
+    restClient.performRequestAsync(request, new ResponseListener() {
+
+      @Override
+      public void onSuccess(Response response) {
+        try {
+          String responseBody = EntityUtils.toString(response.getEntity()); 
+          log.info("*** Elastic *** old index-stats:\n" + responseBody);
+        } catch (Exception error) {
+          log.info("*** Elastic *** ERROR parsing old index-stats: " + error.getMessage());
+        }
+      }
+
+      @Override
+      public void onFailure(Exception error) {
+        log.info("*** Elastic *** ERROR reading old index-stats: " + error.getMessage());
+      }
+    });
+
     return restClient;
   }
   
   @Bean
   public Gson getGson(){
-      return new Gson();
+      return gson;
   }
 }
